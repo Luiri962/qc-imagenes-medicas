ba5ba69b15229dda756caea016c9e9b3edc66787
Luiri962 <l.calderonr96@gmail.com>
2026-05-04 12:03:48 -0500

    Update mtf.py

--- modulos/rayos_x/mtf.py
+++ modulos/rayos_x/mtf.py
@@ -180,9 +180,9 @@ def roi_sobre_borde(img, lado, tipo, nombre_lado, cy_cuad, cx_cuad, px_mm=0.15):
     """
 
     # Después (correcto)
-    profundo = int(lado["largo"] * 0.1)          # 10 mm hacia adentro
+    profundo = int(lado["largo"] * 0.25)          # 10 mm hacia adentro
     margen   = int(-5.0  / px_mm)          # 3 mm de separación desde el borde
-    ancho    = int(7.0 / px_mm)  # 35% del largo del borde
+    ancho    = int(10.0 / px_mm)  # 35% del largo del borde
     cy       = lado["centro_y"]
     cx       = lado["centro_x"]
     H, W     = img.shape
